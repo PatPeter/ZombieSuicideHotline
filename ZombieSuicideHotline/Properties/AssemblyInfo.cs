2318ef0021cd8eea17c51020fce65d176c284c2d
Nicholas Solin <solin.nick@gmail.com>
2020-12-22 18:33:43 -0600

    Update version number to 1.6.6

--- ZombieSuicideHotline/Properties/AssemblyInfo.cs
+++ ZombieSuicideHotline/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ namespace ZombieSuicideHotline
 		/// <summary>
 		/// The AssemblyFileVersion of this web part
 		/// </summary>
-		internal const string Version = "1.6.5.58";
+		internal const string Version = "1.6.6.60";
 	}
 }
